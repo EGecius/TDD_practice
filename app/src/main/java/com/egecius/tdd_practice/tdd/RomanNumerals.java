e3cdfa655f1510949a01af30488fdc1797d4cd79
Egidijus Gecius <e.gecius@gmail.com>
2018-01-23 11:18:43 +0000

    extract all numbers into an algorithm

--- app/src/main/java/com/egecius/tdd_practice/tdd/RomanNumerals.java
+++ app/src/main/java/com/egecius/tdd_practice/tdd/RomanNumerals.java
@@ -7,26 +7,10 @@ public class RomanNumerals {
         StringBuilder stringBuilder = new StringBuilder();
 
         remainder = catchAll(remainder, stringBuilder, 10, "X");
-
-        if (remainder == 9) {
-            String roman9 = "IX";
-            stringBuilder.append(roman9);
-            remainder = remainder - 9;
-        }
-
+        remainder = catchAll(remainder, stringBuilder, 9, "IX");
         remainder = catchAll(remainder, stringBuilder, 5, "V");
-
-        String roman4 = "IV";
-        int noToCatch4 = 4;
-
-        if (remainder == noToCatch4) {
-            stringBuilder.append(roman4);
-            remainder = remainder - noToCatch4;
-        }
-
-        for (int j = 0; j < remainder; j++) {
-            stringBuilder.append('I');
-        }
+        remainder = catchAll(remainder, stringBuilder, 4, "IV");
+        remainder = catchAll(remainder, stringBuilder, 1, "I");
 
         return stringBuilder.toString();
     }
